8a28e1c0c8779e12e9794568d6e48d8482549b6e
SAJAL JAIN <sjain30sept@gmail.com>
2020-07-17 17:13:31 +0530

    Initial commit

---
+++ app/src/main/java/com/sajal/retrofittutorial/Post.java
@@ -0,0 +1,29 @@
+package com.sajal.retrofittutorial;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Post {
+
+    private int userid;
+    private int id;
+    private String title;
+    @SerializedName("body")
+    private String text;
+
+    public int getUserid() {
+        return userid;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getText() {
+        return text;
+    }
+}
+
