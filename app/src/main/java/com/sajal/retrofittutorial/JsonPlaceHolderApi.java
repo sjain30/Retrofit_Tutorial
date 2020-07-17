89c4f2f7d85bd64537507f5b7a3775f45fd3b1d7
SAJAL JAIN <sjain30sept@gmail.com>
2020-07-17 18:36:26 +0530

    quering

--- app/src/main/java/com/sajal/retrofittutorial/JsonPlaceHolderApi.java
+++ app/src/main/java/com/sajal/retrofittutorial/JsonPlaceHolderApi.java
@@ -1,12 +1,30 @@
 package com.sajal.retrofittutorial;
 
 import java.util.List;
+import java.util.Map;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+import retrofit2.http.QueryMap;
+import retrofit2.http.Url;
 
 public interface JsonPlaceHolderApi {
 
+//    @GET("posts")
+//    Call<List<Post>> getPost(
+//            @Query("userId") Integer[] userId,
+//            @Query("_sort") String sort,
+//            @Query("_order") String order);
+
     @GET("posts")
-    Call<List<Post>> getPost();
+    Call<List<Post>> getPost(
+            @QueryMap Map<String,String> parameters);
+
+//    @GET("posts/{id}/comments")
+//    Call<List<Comment>> getComments(@Path("id") int postId);
+
+    @GET
+    Call<List<Comment>> getComments(@Url String url);
 }
