67cf265c6f5988683fc7712f06979f475d908dcb
SAJAL JAIN <sjain30sept@gmail.com>
2020-07-20 11:16:20 +0530

    PUT, PATCH AND DELETE

--- app/src/main/java/com/sajal/retrofittutorial/JsonPlaceHolderApi.java
+++ app/src/main/java/com/sajal/retrofittutorial/JsonPlaceHolderApi.java
@@ -5,11 +5,14 @@ import java.util.Map;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.Field;
 import retrofit2.http.FieldMap;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.PATCH;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
 import retrofit2.http.QueryMap;
@@ -25,13 +28,16 @@ public interface JsonPlaceHolderApi {
 
     @GET("posts")
     Call<List<Post>> getPost(
-            @QueryMap Map<String,String> parameters);
+            @QueryMap Map<String, String> parameters);
+
+    @GET("posts/{id}")
+    Call<Post> getPosts(@Path("id") int postId);
 
 //    @GET("posts/{id}/comments")
 //    Call<List<Comment>> getComments(@Path("id") int postId);
 
     @GET
-    Call<List<Comment>> getComments(@Url String url );
+    Call<List<Comment>> getComments(@Url String url);
 
     @POST("posts")
     Call<Post> createPost(@Body Post post);
@@ -46,5 +52,14 @@ public interface JsonPlaceHolderApi {
 
     @FormUrlEncoded
     @POST("posts")
-    Call<Post> createPost(@FieldMap Map<String,String> fields);
+    Call<Post> createPost(@FieldMap Map<String, String> fields);
+
+    @PUT("posts/{id}")
+    Call<Post> putPost(@Path("id") int id, @Body Post post);
+
+    @PATCH("posts/{id}")
+    Call<Post> patchPost(@Path("id") int id, @Body Post post);
+
+    @DELETE("posts/{id}")
+    Call<Void> deletePost(@Path("id") int id);
 }
--- app/src/main/java/com/sajal/retrofittutorial/MainActivity.java
+++ app/src/main/java/com/sajal/retrofittutorial/MainActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -27,13 +30,62 @@ public class MainActivity extends AppCompatActivity {
 
         textViewResult = findViewById(R.id.text_veiew_result);
 
+        //To force null values in patch request
+        Gson gson = new GsonBuilder().serializeNulls().create();
+
         Retrofit retrofit = new Retrofit.Builder().baseUrl("https://jsonplaceholder.typicode.com/")
-                .addConverterFactory(GsonConverterFactory.create()).build();
+                .addConverterFactory(GsonConverterFactory.create(gson)).build();
 
         jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
 //        getPosts();
 //        getComments();
-        createPosts();
+//        createPosts();
+//        updatePost();
+        deletePost();
+    }
+
+    private void deletePost() {
+        Call<Void> call = jsonPlaceHolderApi.deletePost(5);
+        call.enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                textViewResult.setText("Code: "+response.code());
+            }
+
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+                textViewResult.setText(t.getMessage());
+            }
+        });
+    }
+
+    private void updatePost() {
+        Post post = new Post(12, null, "New Text");
+        Call<Post> call = jsonPlaceHolderApi.putPost(5, post);
+        call.enqueue(new Callback<Post>() {
+            @Override
+            public void onResponse(Call<Post> call, Response<Post> response) {
+                if (!response.isSuccessful()) {
+                    textViewResult.setText("Code: z" + response.code());
+                }
+
+                Post post1 = response.body();
+
+                String content = "";
+                content += "Code: " + response.code() + "\n";
+                content += "ID: " + post1.getId() + "\n";
+                content += "User ID: " + post1.getUserid() + "\n";
+                content += "Title: " + post1.getTitle() + "\n";
+                content += "Text: " + post1.getText() + "\n";
+
+                textViewResult.setText(content);
+            }
+
+            @Override
+            public void onFailure(Call<Post> call, Throwable t) {
+                textViewResult.setText(t.getMessage());
+            }
+        });
     }
 
     private void createPosts() {
@@ -41,8 +93,8 @@ public class MainActivity extends AppCompatActivity {
         Post post = new Post(23, "New Title", "New text");
 
         Map<String, String> params = new HashMap<>();
-        params.put("userId","23");
-        params.put("title","New Title");
+        params.put("userId", "23");
+        params.put("title", "New Title");
 
         Call<Post> call = jsonPlaceHolderApi.createPost(params);
         call.enqueue(new Callback<Post>() {
@@ -130,33 +182,53 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void getPosts() {
-//        Call<List<Post>> call = jsonPlaceHolderApi.getPost(new Integer[]{2,3,4},null,null);
+//        Call<List<Post>> call = jsonPlaceHolderApi.getPosts(5);
         Map<String, String> parameters = new HashMap<>();
         parameters.put("userId", "2");
         parameters.put("_sort", "id");
         parameters.put("_order", "desc");
-        Call<List<Post>> call = jsonPlaceHolderApi.getPost(parameters);
+//        Call<List<Post>> call = jsonPlaceHolderApi.getPost(parameters);
 
-        call.enqueue(new Callback<List<Post>>() {
+//        call.enqueue(new Callback<List<Post>>() {
+//            @Override
+//            public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
+//                if (!response.isSuccessful()) {
+//                    textViewResult.setText("Code: " + response.code());
+//                    return;
+//                }
+//
+//                List<Post> posts = response.body();
+//
+//                for (Post post : posts
+//                ) {
+//                    String content = "";
+//                    content += "User ID: " + post.getUserid() + "\nID: " + post.getId() + "\n" + "Title: " + post.getTitle() + "\nText: " + post.getText() + "\n\n";
+//                    textViewResult.append(content);
+//                }
+//            }
+//
+//            @Override
+//            public void onFailure(Call<List<Post>> call, Throwable t) {
+//                textViewResult.setText(t.getMessage());
+//            }
+//        });
+
+        Call<Post> call = jsonPlaceHolderApi.getPosts(5);
+        call.enqueue(new Callback<Post>() {
             @Override
-            public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
+            public void onResponse(Call<Post> call, Response<Post> response) {
                 if (!response.isSuccessful()) {
                     textViewResult.setText("Code: " + response.code());
                     return;
                 }
-
-                List<Post> posts = response.body();
-
-                for (Post post : posts
-                ) {
-                    String content = "";
-                    content += "User ID: " + post.getUserid() + "\nID: " + post.getId() + "\n" + "Title: " + post.getTitle() + "\nText: " + post.getText() + "\n\n";
-                    textViewResult.append(content);
-                }
+                String content = "";
+                Post post = response.body();
+                content += "User ID: " + post.getUserid() + "\nID: " + post.getId() + "\n" + "Title: " + post.getTitle() + "\nText: " + post.getText() + "\n\n";
+                textViewResult.append(content);
             }
 
             @Override
-            public void onFailure(Call<List<Post>> call, Throwable t) {
+            public void onFailure(Call<Post> call, Throwable t) {
                 textViewResult.setText(t.getMessage());
             }
         });
